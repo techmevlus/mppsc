1fea2e9de1c02f303c59ebf5bc28d9bd3773cffb
akashsaste5 <waterearth.me@gmail.com>
2021-05-18 23:04:56 +0530

    Admin Authentication

--- src/AdminSector/AdminLogin.js
+++ src/AdminSector/AdminLogin.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from '../Utils/Common';
  
-function AdminLogin(props) {
+function Login(props) {
+  const [loading, setLoading] = useState(false);
   const username = useFormInput('');
   const password = useFormInput('');
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
  
   // handle button click of login form
   const handleLogin = () => {
-    props.history.push('/dashboard');
+    setError(null);
+    setLoading(true);
+    axios.post('http://localhost:3001/users/signin', { username: username.value, password: password.value }).then(response => {
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.");
+    });
   }
- 
   return (
     <div>
       Login<br /><br />
@@ -40,4 +51,4 @@ const useFormInput = initialValue => {
   }
 }
  
-export default AdminLogin;
+export default Login;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -83,9 +84,9 @@ router.route('/questions')
 			 res.json(dataFromDB);
 			 console.log(dataFromDB)
 		 });
-	  })
+	  });
 	  //post new admin info to the database
-	  .post(function(req, res) {
+	 /* .post(function(req, res) {
 		 
 		console.log("post method working...");
 		var cred 		= new Cred();
@@ -98,8 +99,7 @@ router.route('/questions')
 			//res.json({ message:'Admin successfully added!' });
 			console.log(res);
 		});
-		console.log("ending here")
-	});
+	});*/
 
 const mongo = require('mongodb').MongoClient;
 mongo.connect('mongodb+srv://techmevlus:0000@cluster0.xt9zo.mongodb.net/mppsc_db?retryWrites=true&w=majority', function(err, client) {
@@ -120,6 +120,121 @@ db.listCollections().toArray(function(err, collections) {
 });
 
 
+//User Authentication Here
+
+//require('dotenv').config();
+
+//const express = require('express');
+const cors = require('cors');
+//const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const utils = require('./utils');
+
+//const app = express();
+//const port = process.env.PORT || 4000;
+
+// enable CORS
+app.use(cors());
+// parse application/json
+//app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+// static user details
+const userData = {
+	//userId: "789789",
+	username: "kuldeep",
+	//name: "Clue Mediator",
+	password: "qaz",
+	//isAdmin: true
+  };
+
+//middleware that checks if JWT token exists and verifies it if it does exist.
+//In all future routes, this helps to know if the request is authenticated or not.
+app.use(function (req, res, next) {
+	// check header or url parameters or post parameters for token
+	var token = req.headers['authorization'];
+	if (!token) return next(); //if no token, continue
+   
+	token = token.replace('Bearer ', '');
+	jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+	  if (err) {
+		return res.status(401).json({
+		  error: true,
+		  message: "Invalid user."
+		});
+	  } else {
+		req.user = user; //set the user to req so other routes can use it
+		next();
+	  }
+	});
+  });
+   
+   
+  // request handlers
+  app.get('/', (req, res) => {
+	if (!req.user) return res.status(401).json({ success: false, message: 'Invalid user to access it.' });
+	res.send('Welcome to the Node.js Tutorial! - ' + req.user.name);
+  });
+   
+   
+  // validate the user credentials
+  app.post('/users/signin', function (req, res) {
+	const user = req.body.username;
+	const pwd = req.body.password;
+	// return 400 status if username/password is not exist
+	if (!user || !pwd) {
+	  return res.status(400).json({
+		error: true,
+		message: "Username or Password required."
+	  });
+	}
+   
+	// return 401 status if the credential is not match.
+	if (user !== userData.username || pwd !== userData.password) {
+	  return res.status(401).json({
+		error: true,
+		message: "Username or Password is Wrong.",
+	  });
+	}
+	// generate token
+	const token = utils.generateToken(userData);
+	// get basic user details
+	const userObj = utils.getCleanUser(userData);
+	// return the token along with user details
+	return res.json({ user: userObj, token });
+  });
+   
+   
+  // verify the token and return it if it's valid
+  app.get('/verifyToken', function (req, res) {
+	// check header or url parameters or post parameters for token
+	var token = req.body.token || req.query.token;
+	if (!token) {
+	  return res.status(400).json({
+		error: true,
+		message: "Token is required."
+	  });
+	}
+	// check token that was passed by decoding token using secret
+	jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+	  if (err) return res.status(401).json({
+		error: true,
+		message: "Invalid token."
+	  });
+   
+	  // return 401 status if the userId does not match.
+	  if (user.userId !== userData.userId) {
+		return res.status(401).json({
+		  error: true,
+		  message: "Invalid user."
+		});
+	  }
+	  // get basic user details
+	  var userObj = utils.getCleanUser(userData);
+	  return res.json({ user: userObj, token });
+	});
+  });
 	
 app.listen(3001, function() {
 	console.log('Api successfully running');
